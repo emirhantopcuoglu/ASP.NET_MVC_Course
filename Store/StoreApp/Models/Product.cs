a6cce6ef837324d8773ad9f394cf0d495b9b2624
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2023-07-15 12:22:42 +0300

    Repository Tanimi yapildi

---
+++ Store/StoreApp/Models/Product.cs
@@ -0,0 +1,7 @@
+namespace StoreApp.Models{
+    public class Product{
+        public int ProuctId { get; set; }
+        public String? ProductName { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
