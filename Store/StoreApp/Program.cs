ca31c1e33247e012e0d1221af47a409475d4422b
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2023-07-15 13:10:54 +0300

    ConnectionString

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,12 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); // Servis kaydı eklendi
+builder.Services.AddDbContext<RepositoryContext>(options=>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
 
 var app = builder.Build();
 
 app.UseHttpsRedirection(); // Redirection
 app.UseRouting(); // Routing 
 
-app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}"); 
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
