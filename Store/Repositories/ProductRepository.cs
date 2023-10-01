9bdbbc1860ff11203e9460d5c97783948debd620
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2023-10-01 09:36:16 +0300

    FilterByCategoryId

--- Store/Repositories/ProductRepository.cs
+++ Store/Repositories/ProductRepository.cs
@@ -1,4 +1,6 @@
 using Entities.Models;
+using Entities.RequestParameters;
+using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
 
 namespace Repositories
@@ -15,6 +17,18 @@ namespace Repositories
         public void DeleteOneProduct(Product product) => Remove(product);
 
         public IQueryable<Product> GetAllProducts(bool trackChanges) => FindAll(trackChanges);
+
+        public IQueryable<Product> GetAllProductsWithDetails(ProductRequestParameters p)
+        {
+            return p.CategoryId is null
+            ? _context
+                .Products
+                .Include(prd => prd.Category)
+            : _context
+                .Products
+                .Include(prd => prd.Category)
+                .Where(prd => prd.CategoryId.Equals(p.CategoryId));
+        }
 
         // Interface
         public Product? GetOneProduct(int id, bool trackChanges)
