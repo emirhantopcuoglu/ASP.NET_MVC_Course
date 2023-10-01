2d363ad44a5c0de38506dfc4b1c795f4541054eb
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2023-10-01 17:24:01 +0300

    Product_Search_Extension_Method

--- Store/Repositories/Extensions/ProductRepositoryExtension.cs
+++ Store/Repositories/Extensions/ProductRepositoryExtension.cs
@@ -4,13 +4,22 @@ namespace Repositories.Extensions
 {
     public static class ProductRepositoryExtension
     {
-        public static IQueryable<Product> FilteredByCategoryId(this IQueryable<Product> products, 
+        public static IQueryable<Product> FilteredByCategoryId(this IQueryable<Product> products,
             int? categoryId)
         {
-            if(categoryId is null)
+            if (categoryId is null)
                 return products;
             else
                 return products.Where(prd => prd.CategoryId.Equals(categoryId));
+        }
+        public static IQueryable<Product> FilteredBySearchTerm(this IQueryable<Product> products,
+            String? searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+                return products;
+            else
+                return products.Where(prd => prd.ProductName.ToLower()
+                    .Contains(searchTerm.ToLower()));
         }
     }
 }
