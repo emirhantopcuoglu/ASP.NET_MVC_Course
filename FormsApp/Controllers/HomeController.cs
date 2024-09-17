e5f2898fcdad20354dab541b0920f6102af4cc0e
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-09-17 21:47:06 +0300

    ProductViewModel added

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -25,8 +25,14 @@ public class HomeController : Controller
             products = products.Where(x => x.CategoryId == int.Parse(category)).ToList();
         }        
 
-        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
-        return View(products);
+        // ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name", category);
+
+        var model = new ProductViewModel{
+            Products = products,
+            Categories = Repository.Categories,
+            SelectedCategory = category
+        };
+        return View(model);
     }
 
     public IActionResult Privacy()
