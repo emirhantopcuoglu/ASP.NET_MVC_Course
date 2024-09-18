584f49024eb9fabd158b187f4dbb332538908bd9
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-09-18 19:57:08 +0300

    Model Binding

--- FormsApp/Controllers/HomeController.cs
+++ FormsApp/Controllers/HomeController.cs
@@ -44,7 +44,8 @@ public class HomeController : Controller
     [HttpPost]
     public IActionResult Create(Product model)
     {
-        return View();
+        Repository.CreateProduct(model);
+        return RedirectToAction("Index");
     }
 
     
