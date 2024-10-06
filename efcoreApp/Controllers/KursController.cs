e41f277a71f529c694c74f5bda0118b4d0a59e80
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-10-06 15:15:55 +0300

    Course Edit Page

--- efcoreApp/Controllers/KursController.cs
+++ efcoreApp/Controllers/KursController.cs
@@ -31,6 +31,54 @@ namespace efcoreApp.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var kurs = await _context.Kurslar.FindAsync(id); 
+            
+            if (kurs == null)
+            {
+                return NotFound();
+            }
+            return View(kurs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Kurs model)
+        {
+            if (id != model.KursId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (_context.Kurslar.Any(o => o.KursId != model.KursId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
         
     }
 }
