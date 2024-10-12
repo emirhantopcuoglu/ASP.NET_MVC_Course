7e644cfbeb3ca5309fcd8836149a23ef705f5a53
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-10-12 14:17:02 +0300

    Migrations created for BlogApp

--- BlogApp/BlogApp/Program.cs
+++ BlogApp/BlogApp/Program.cs
@@ -2,14 +2,14 @@ using BlogApp.Data.Concrete.EfCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddDbContext<BlogContext>(options => {
     var config = builder.Configuration;
     var connectionString = config.GetConnectionString("sql_connection");
-    options.UseSqlite();
+    options.UseSqlite(connectionString);
 });
 
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
