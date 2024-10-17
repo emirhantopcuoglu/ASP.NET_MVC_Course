8feda5f433961479e6f882f1e5eb53c7c75f0044
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-10-17 20:41:49 +0300

    View Components created

--- BlogApp/BlogApp/Models/PostsViewModel.cs
+++ BlogApp/BlogApp/Models/PostsViewModel.cs
@@ -5,6 +5,6 @@ namespace BlogApp.Models
     public class PostsViewModel
     {
         public List<Post> Posts { get; set; } = new List<Post>();
-        public List<Tag> Tags { get; set; } = new List<Tag>();
+
     }
 }
