1806f83439aed748307d9fb214150b6c62451b01
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-10-18 21:17:11 +0300

    Post List by Tag

--- BlogApp/BlogApp/Program.cs
+++ BlogApp/BlogApp/Program.cs
@@ -27,6 +27,12 @@ app.MapControllerRoute(
 );
 
 app.MapControllerRoute(
+    name: "posts_by_tag",
+    pattern: "posts/tag/{url}",
+    defaults: new { controller = "Posts", action = "Index"}
+);
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"
 );
