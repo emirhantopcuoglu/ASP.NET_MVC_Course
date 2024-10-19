38d70351423cc65c389b3e6015b4e122462e137b
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-10-19 14:45:18 +0300

    Recording Comment

--- BlogApp/BlogApp/Controllers/PostsController.cs
+++ BlogApp/BlogApp/Controllers/PostsController.cs
@@ -1,5 +1,6 @@
 using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete.EfCore;
+using BlogApp.Entity;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,9 +9,11 @@ namespace BlogApp.Controllers
     public class PostsController : Controller
     {
         private IPostRepository _postRepository;
-        public PostsController(IPostRepository postRepository)
+        private ICommentRepository _commentRepository;  
+        public PostsController(IPostRepository postRepository, ICommentRepository commentRepository)
         {
             _postRepository = postRepository;
+            _commentRepository = commentRepository;
         }
         public async Task<IActionResult> Index(string tag)
         {
@@ -32,9 +35,18 @@ namespace BlogApp.Controllers
             .FirstOrDefaultAsync(p => p.Url == url));
         }
 
-        public IActionResult AddComment(int PostId, string UserName, string Text)
+        public IActionResult AddComment(int PostId, string UserName, string Text, string Url)
         {
-            return View();
+            var entity = new Comment {
+                Text = Text,
+                PublishedOn = DateTime.Now,
+                PostId = PostId,    
+                User = new User { UserName = UserName, Image = "cat.jpg"}
+            };
+            _commentRepository.CreateComment(entity);
+
+            //return Redirect("/posts/details/" + Url);
+            return RedirectToAction("post_details", new { url = Url });
         }
     }
 }
