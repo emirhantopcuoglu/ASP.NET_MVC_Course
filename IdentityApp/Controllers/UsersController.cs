57210de243b323984e88f09ffc70fc82fbcf2155
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-07 16:04:52 +0300

    Create User

--- IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/Controllers/UsersController.cs
@@ -1,18 +1,46 @@
+using IdentityApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
 {
-    public class UsersController : Controller
+    public class UsersController:Controller
     {
         private UserManager<IdentityUser> _userManager;
-        public UsersController(UserManager<IdentityUser> userManager)
+        public UsersController(UserManager<IdentityUser>  userManager)
         {
             _userManager = userManager;
         }
         public IActionResult Index()
         {
             return View(_userManager.Users);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.UserName, Email = model.Email };
+
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);                    
+                }
+            }
+            return View(model);
         }
     }
 }
