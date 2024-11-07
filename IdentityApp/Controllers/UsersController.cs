9a63cb059a2c80a18eeeb5e72b2a4f40d170a2fb
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-07 17:07:33 +0300

    User Edit Form

--- IdentityApp/Controllers/UsersController.cs
+++ IdentityApp/Controllers/UsersController.cs
@@ -5,10 +5,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
 {
-    public class UsersController:Controller
+    public class UsersController : Controller
     {
         private UserManager<AppUser> _userManager;
-        public UsersController(UserManager<AppUser>  userManager)
+        public UsersController(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
         }
@@ -25,23 +25,45 @@ namespace IdentityApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateViewModel model)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 var user = new AppUser { UserName = model.Email, Email = model.Email, FullName = model.FullName };
 
                 IdentityResult result = await _userManager.CreateAsync(user, model.Password);
 
-                if(result.Succeeded)
+                if (result.Succeeded)
                 {
                     return RedirectToAction("Index");
                 }
 
                 foreach (IdentityError err in result.Errors)
                 {
-                    ModelState.AddModelError("", err.Description);                    
+                    ModelState.AddModelError("", err.Description);
                 }
             }
             return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+
+                return View(new EditViewModel
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email
+                });
+            }
+            return RedirectToAction("Index");
         }
     }
 }
