123347f4700c3c0876706697d81979c08766a52d
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-15 12:33:23 +0300

    Listing Roles on User Detail Page

--- IdentityApp/ViewModels/EditViewModel.cs
+++ IdentityApp/ViewModels/EditViewModel.cs
@@ -17,5 +17,7 @@ namespace IdentityApp.ViewModels
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Parola eşleşmiyor.")]
         public string? ConfirmPassword { get; set; }
+
+        public IList<string>? SelectedRoles { get; set; }
     }
 }
