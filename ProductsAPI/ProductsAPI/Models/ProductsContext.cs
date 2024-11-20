a9f9da28f3d1b3f0ee83dc116416809664195f39
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-20 10:53:35 +0300

    ProductsAPI : Database created

---
+++ ProductsAPI/ProductsAPI/Models/ProductsContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductsAPI.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext(DbContextOptions<ProductsContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 1, ProductName = "IPhone 14", Price = 60000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 2, ProductName = "IPhone 15", Price = 70000, IsActive = false });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 3, ProductName = "IPhone 16", Price = 80000, IsActive = true });
+            modelBuilder.Entity<Product>().HasData(new Product() { ProductId = 4, ProductName = "IPhone 17", Price = 90000, IsActive = false });
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
