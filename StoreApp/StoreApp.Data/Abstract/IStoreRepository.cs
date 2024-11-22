f36b4c67744cd633820491acb4c8b69b3d0d93e4
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-22 19:43:48 +0300

    StoreApp: Repository created

---
+++ StoreApp/StoreApp.Data/Abstract/IStoreRepository.cs
@@ -0,0 +1,10 @@
+using StoreApp.Data.Concrete;
+
+namespace StoreApp.Data.Abstract
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Product> Products { get; }
+        void CreateProduct(Product entity);
+    }
+}
--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using StoreApp.Data.Abstract;
 
 namespace Name
 {
     public class HomeController : Controller
     {
+        private IStoreRepository _storeRepository;
+        public HomeController(IStoreRepository storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
+        
         public IActionResult Index()
         {
             return View();
