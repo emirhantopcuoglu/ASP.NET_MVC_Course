0b472aa0be026305cf8c13d818d66b58882156f0
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-25 13:44:54 +0300

    Product List

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using StoreApp.Data.Abstract;
+using StoreApp.Web.Models;
 
 namespace StoreApp.Web.Controllers;
-public class HomeController :Controller
+public class HomeController : Controller
 {
     private IStoreRepository _storeRepository;
     public HomeController(IStoreRepository storeRepository)
     {
         _storeRepository = storeRepository;
     }
-    public IActionResult Index() => View();
+    public IActionResult Index()
+    {
+        var products = _storeRepository.Products.Select(p => new ProductViewModel
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Description = p.Description,
+            Price = p.Price
+        }).ToList();
+        return View(new ProductListViewModel
+        {
+            Products = products
+        });
+    }
 }
