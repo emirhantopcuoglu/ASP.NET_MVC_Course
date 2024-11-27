5be7109841b349f6031ee3e377432a38186f0a38
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-11-27 18:27:30 +0300

    Pagination Links

--- StoreApp/StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp/StoreApp.Web/Controllers/HomeController.cs
@@ -26,7 +26,11 @@ public class HomeController : Controller
             }).Take(pageSize);
         return View(new ProductListViewModel
         {
-            Products = products
+            Products = products,
+            PageInfo = new PageInfo{
+                ItemsPerPage = pageSize,
+                TotalItems = _storeRepository.Products.Count()
+            }
         });
     }
 }
